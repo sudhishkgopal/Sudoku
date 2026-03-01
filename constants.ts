3dced876875c9acb32ce9c563537b9e289b1e2c3
Sudhish Gopalakrishnan <sudhish.gk1@gmail.com>
2026-03-01 15:56:53 -0500

    Added grid constants

---
+++ constants.ts
@@ -0,0 +1,10 @@
+//Grid constants
+export const GRID_SIZE = 9;
+export const BOX_SIZE = 3;
+export const CELL_COUNT = GRID_SIZE * GRID_SIZE;
+export const BOX_COUNT = GRID_SIZE / BOX_SIZE;
+export const DIGITS: readonly number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+export const EMPTY_CELL = 0; //0 = blank cell
+
+//Graph constants
+
